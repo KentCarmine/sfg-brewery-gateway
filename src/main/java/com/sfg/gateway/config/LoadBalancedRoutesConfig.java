03659eb67db5dec3624564ca222384fb6bbbc637
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-04-06 16:25:34 -0500

    Resolved dependency incompatability issue by updating pom

--- src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -20,4 +20,20 @@ public class LoadBalancedRoutesConfig {
                         .uri("lb://inventory-service"))
                 .build();
     }
+
+//    @Bean
+//    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+//                        .uri("lb://beer-service")
+//                        .id("beer-service"))
+//                .route(r -> r.path("/api/v1/customers/**")
+//                        .uri("lb://order-service")
+//                        .id("order-service"))
+//                .route(r -> r.path("/api/v1/beer/*/inventory")
+//                        .uri("lb://inventory-service")
+//                        .id("inventory-service"))
+//                .build();
+//    }
+
 }
