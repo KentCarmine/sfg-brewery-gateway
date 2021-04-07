525f639d3d604a8f11d744dc2d2cc4bfd83c25a3
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2021-04-07 16:34:12 -0500

    Added circuit breaker failover to inventory-service

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -14,7 +14,7 @@ public class LocalHostRouteConfig {
     public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                    .uri("http://localhost:8080"))
+                        .uri("http://localhost:8080"))
                 .route("order-service", r -> r.path("/api/v1/customers/**")
                         .uri("http://localhost:8081"))
                 .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
